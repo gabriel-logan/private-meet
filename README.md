e5a11d07a0cdd8ea13f58b94814a00bf0d6eccce
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-09-14 14:43:07 -0300

    feat: enhance ESLint configuration with simple-import-sort plugin and update rules

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # private-meet
+
 private-meet
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,34 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ["eslint.config.mjs"],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  {
+    plugins: { "simple-import-sort": simpleImportSort },
+    rules: {
+      "simple-import-sort/imports": [
+        "warn",
+        {
+          groups: [["^\\u0000"], ["^@?\\w"], ["^@company/"], ["^"], ["^\\."]],
+        },
+      ],
+    },
+  },
   {
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: "commonjs",
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -26,9 +37,36 @@ export default tseslint.config(
   },
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      "no-console": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/prefer-readonly": "error",
+      "no-else-return": ["error", { allowElseIf: false }],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "class",
+          format: ["PascalCase"],
+        },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/consistent-type-exports": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        {
+          accessibility: "explicit",
+          overrides: {
+            accessors: "explicit",
+            constructors: "no-public",
+            methods: "no-public",
+            properties: "explicit",
+            parameterProperties: "explicit",
+          },
+        },
+      ],
+      "@typescript-eslint/no-unnecessary-condition": "error",
     },
   },
 );
