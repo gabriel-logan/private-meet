43fecc515e49c3086bcb2f335a2c528453666d99
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-09-14 17:47:12 -0300

    feat: add room ID input and buttons for joining and creating chat rooms

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,2 +1,33 @@
-// eslint-disable-next-line no-console
-console.log("oai");
+import type { CreateRoomDto } from "src/chat/dto/create-room.dto";
+
+const roomIdInput = document.getElementById(
+  "room-id-input",
+) as HTMLInputElement;
+
+const joinRoomBtn = document.getElementById(
+  "join-room-button",
+) as HTMLButtonElement;
+
+const createRoomBtn = document.getElementById(
+  "create-room-button",
+) as HTMLButtonElement;
+
+joinRoomBtn.addEventListener("click", () => {
+  if (roomIdInput.value.trim() === "") {
+    return alert("Please enter a room ID");
+  }
+
+  window.location.href = `/chat?roomId=${roomIdInput.value.trim()}`;
+});
+
+createRoomBtn.addEventListener("click", () => {
+  void handleCreateRoom();
+});
+
+async function handleCreateRoom(): Promise<void> {
+  const response = await fetch("/chat/generate-room", { method: "POST" });
+
+  const generatedRoomId = (await response.json()) as CreateRoomDto;
+
+  window.location.href = `/chat?roomId=${generatedRoomId.roomId}`;
+}
