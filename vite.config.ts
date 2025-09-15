274be41bac163a1ded69468207d1f7751f7f5d6c
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-09-15 20:02:59 -0300

    feat: add public/assets to .gitignore and configure path alias in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      src: resolve(__dirname, "src"),
+    },
+  },
   build: {
     emptyOutDir: false,
     rollupOptions: {
