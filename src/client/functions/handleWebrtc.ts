2482bc7758ec46b10f70b39bc8c55243cfb9e292
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-09-22 15:58:28 -0300

    fix: add additional STUN servers for improved connectivity

--- src/client/functions/handleWebrtc.ts
+++ src/client/functions/handleWebrtc.ts
@@ -25,7 +25,12 @@ interface HandleWebrtcParams {
 const rtcConfig: RTCConfiguration = {
   iceServers: [
     {
-      urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"],
+      urls: [
+        "stun:stun.l.google.com:19302",
+        "stun:stun1.l.google.com:19302",
+        "stun:stun2.l.google.com:19302",
+        "stun:stun3.l.google.com:19302",
+      ],
     },
   ],
 };
