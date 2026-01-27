d7640cc2fd9c6d137675e5424ecc40ec7c9fcc71
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-01-27 20:21:34 -0300

    feat: add WebSocket instance management and configuration

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+
+import { initWSInstance } from "./lib/wsInstance";
 import Router from "./Router";
 
 function App() {
+  useEffect(() => {
+    initWSInstance("awdawd");
+  }, []);
+
   return <Router />;
 }
 
---
+++ web/src/lib/wsInstance.ts
@@ -0,0 +1,55 @@
+let ws: WebSocket | null = null;
+
+const baseURL = import.meta.env.VITE_WS_API_URL as string;
+
+export function initWSInstance(token?: string): WebSocket {
+  if (ws) {
+    return ws;
+  }
+
+  const url = new URL(baseURL);
+
+  url.protocol = url.protocol.replace("http", "ws");
+
+  url.pathname = "/ws";
+
+  if (token) {
+    url.searchParams.append("token", token);
+  }
+
+  ws = new WebSocket(url.toString());
+
+  return ws;
+}
+
+export function updateWSInstanceToken(token: string): WebSocket {
+  if (!ws) {
+    return initWSInstance(token);
+  }
+
+  if (
+    ws.readyState === WebSocket.OPEN ||
+    ws.readyState === WebSocket.CONNECTING
+  ) {
+    ws.close();
+  }
+
+  ws = null;
+
+  return initWSInstance(token);
+}
+
+export function getWSInstance(): WebSocket {
+  if (!ws) {
+    throw new Error("WebSocket instance is not initialized.");
+  }
+
+  return ws;
+}
+
+export function closeWSInstance() {
+  if (ws) {
+    ws.close();
+    ws = null;
+  }
+}
