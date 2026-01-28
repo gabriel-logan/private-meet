9ab11aaff285735e0dc0180f66326fa7b5117902
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-01-27 21:40:42 -0300

    feat: implement WebSocket server in main.go and update go.mod for gorilla/websocket dependency

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module github.com/gabriel-logan/private-meet/server
 
 go 1.25.0
+
+require github.com/gorilla/websocket v1.5.3
--- server/main.go
+++ server/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 func main() {
-	fmt.Println("Hello, Private Meet!")
+	serverMux := http.NewServeMux()
+
+	serverMux.HandleFunc("GET /ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			http.Error(w, "Failed to upgrade to WebSocket", http.StatusInternalServerError)
+			return
+		}
+		defer conn.Close()
+
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				break
+			}
+
+			err = conn.WriteMessage(websocket.TextMessage, message)
+			if err != nil {
+				break
+			}
+		}
+	})
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: serverMux,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
