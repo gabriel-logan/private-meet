692e30619c61331daabdb91576056589f5888a0b
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-01-28 23:11:08 -0300

    feat: add websocket upgrade handling to logger middleware

--- server/internal/middleware/logger.go
+++ server/internal/middleware/logger.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 type responseWriter struct {
@@ -49,6 +51,11 @@ func clientIP(r *http.Request) string {
 func Logger() Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if websocket.IsWebSocketUpgrade(r) {
+				next.ServeHTTP(w, r)
+				return
+			}
+
 			start := time.Now()
 
 			rw := &responseWriter{
