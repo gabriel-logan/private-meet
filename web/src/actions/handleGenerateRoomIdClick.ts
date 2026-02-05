6ab3a52a0ddd4f8dbe38c82a9f3f5db87e011b7f
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-02-05 17:19:47 -0300

    refactor(handleGenerateRoomIdClick): remove WebSocket connection check before sending message

--- web/src/actions/handleGenerateRoomIdClick.ts
+++ web/src/actions/handleGenerateRoomIdClick.ts
@@ -7,10 +7,6 @@ export default function handleGenerateRoomIdClick() {
   try {
     const ws = getWSInstance();
 
-    if (ws.readyState !== WebSocket.OPEN) {
-      throw new Error("WebSocket is not connected.");
-    }
-
     ws.send(makeWSMessage("utils.generateRoomID"));
   } catch (error) {
     console.error("Error generating Room ID:", error);
