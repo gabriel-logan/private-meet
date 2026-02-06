468bab5b3895d6bb6579de30c334bc199bfad640
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-02-05 23:32:14 -0300

    refactor: update ESLint and Prettier configurations to exclude coverage directory

--- web/src/utils/i18n.ts
+++ web/src/utils/i18n.ts
@@ -6,6 +6,7 @@ import { useUserStore } from "../stores/userStore";
 
 const language = useUserStore.getState().locale;
 
+// eslint-disable-next-line no-console
 console.log("i18n language:", language);
 
 i18next.use(initReactI18next).init({
