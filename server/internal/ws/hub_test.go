dcb9f17f36cf341229c62ce9715c61d6668c8730
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-02-11 06:45:15 -0300

    test: remove tests for client join/leave room functionality

--- server/internal/ws/hub_test.go
+++ server/internal/ws/hub_test.go
@@ -3,16 +3,5 @@ package ws
 import "testing"
 
 func TestHubJoinLeaveRoom(t *testing.T) {
-	h := NewHub()
-	c := &Client{UserID: "u1", Username: "name", send: make(chan []byte, 1)}
 
-	h.clientJoinRoom("room1", c)
-	if !h.isClientInRoom("room1", c) {
-		t.Fatalf("expected client to be in room")
-	}
-
-	h.clientLeaveRoom("room1", c)
-	if h.isClientInRoom("room1", c) {
-		t.Fatalf("expected client to be removed from room")
-	}
 }
