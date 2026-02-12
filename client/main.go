e4a68d3d9f518bfce365ee942476b7a8f8109e17
gabriel-logan <gabrielcoutolima@protonmail.ch>
2026-02-12 01:53:50 -0300

    refactor: update app name to "Private Meet" in main.go and wails.json

--- client/main.go
+++ client/main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "private-meet",
+		Title:  "Private Meet",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
