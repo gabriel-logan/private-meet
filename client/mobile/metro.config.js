2096827853e56a3d560828870bc6da87d0cc900a
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-02-13 20:00:10 -0300

    feat: add watchFolders configuration to Metro for shared folder support

--- client/mobile/metro.config.js
+++ client/mobile/metro.config.js
@@ -1,4 +1,5 @@
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const path = require("node:path");
 
 /**
  * Metro configuration
@@ -6,6 +7,8 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+	watchFolders: [path.resolve(__dirname, "../shared")],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
