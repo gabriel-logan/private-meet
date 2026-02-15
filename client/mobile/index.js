c1a511463d767723f2d7cc5ad8770380afd76a5c
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2026-02-15 09:36:37 -0300

    refactor: reorder imports for better consistency in index.js

--- client/mobile/index.js
+++ client/mobile/index.js
@@ -1,14 +1,16 @@
 import { AppRegistry } from "react-native";
+
 import { install } from "react-native-quick-crypto";
 import { registerGlobals } from "react-native-webrtc";
-
-import App from "./App";
-import { name as appName } from "./app.json";
 
 import "react-native-get-random-values";
 
 install();
 
 registerGlobals();
+
+import App from "./App";
+
+import { name as appName } from "./app.json";
 
 AppRegistry.registerComponent(appName, () => App);
