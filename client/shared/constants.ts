f436d9324c243441f725c24a696aae2f59078263
gabriel-logan <gabrielcoutolima@protonmail.ch>
2026-02-25 02:32:30 -0300

    feat: expand iceServers configuration with additional STUN servers

--- client/shared/constants.ts
+++ client/shared/constants.ts
@@ -1,1 +1,7 @@
-export const iceServers: RTCIceServer[] = [{ urls: "stun:stun.l.google.com:19302" }];
+export const iceServers: RTCIceServer[] = [
+  { urls: "stun:stun.l.google.com:19302" },
+  { urls: "stun:stun1.l.google.com:19302" },
+  { urls: "stun:stun2.l.google.com:19302" },
+  { urls: "stun:global.stun.twilio.com:3478" },
+  { urls: "stun:stun.cloudflare.com:3478" },
+];
