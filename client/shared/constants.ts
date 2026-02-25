5c4cb81577e601c41354c80751a4073e988ed501
gabriel-logan <gabrielcoutolima@protonmail.ch>
2026-02-25 03:37:32 -0300

    fix: remove redundant STUN server entries from iceServers configuration

--- client/shared/constants.ts
+++ client/shared/constants.ts
@@ -1,7 +1,5 @@
 export const iceServers: RTCIceServer[] = [
   { urls: "stun:stun.l.google.com:19302" },
-  { urls: "stun:stun1.l.google.com:19302" },
-  { urls: "stun:stun2.l.google.com:19302" },
   { urls: "stun:global.stun.twilio.com:3478" },
   { urls: "stun:stun.cloudflare.com:3478" },
 ];
